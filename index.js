269a2488d2fa2a1bdd23efe1b17ca3842a46eefd
Manav Misra <manavm@visionwebsoft.com>
2022-04-20 07:43:38 -0500

    Update namings to reflect React more

---
+++ index.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+document.querySelector("#root").innerHTML = `
+<h1 class="text-3xl text-center text-red-500 mt-3">Vite ➕ Tailwind</h1>
+  <p class="text-center"><a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a></p>
+`;
